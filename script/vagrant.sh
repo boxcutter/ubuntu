ac217e95882a8b000ea903fb8d9635cad31abb40
Oliver, Jonathan <git@jonathanoliver.com>
2014-06-09 14:07:36 -0600

    Embedding the insecure Vagrant private key directly into the vagrant.sh script.
    This avoids the somewhat heavy-handed approach of mounting a "floppy" which isn't universally available across other OS instances.

--- script/vagrant.sh
+++ script/vagrant.sh
@@ -4,6 +4,7 @@ date > /etc/vagrant_box_build_time
 
 VAGRANT_USER=vagrant
 VAGRANT_HOME=/home/$VAGRANT_USER
+VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 
 # Create Vagrant user (if not already present)
 if ! id -u $VAGRANT_USER >/dev/null 2>&1; then
@@ -21,12 +22,8 @@ echo "==> Installing vagrant key"
 mkdir $VAGRANT_HOME/.ssh
 chmod 700 $VAGRANT_HOME/.ssh
 cd $VAGRANT_HOME/.ssh
+
 # https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-mkdir -p /mnt/floppy
-modprobe floppy
-mount -t vfat /dev/fd0 /mnt/floppy
-cp /mnt/floppy/vagrant.pub $VAGRANT_HOME/.ssh/authorized_keys
-umount /mnt/floppy
-rmdir /mnt/floppy
+echo "${VAGRANT_INSECURE_KEY}" > $VAGRANT_HOME/.ssh/authorized_keys
 chmod 600 $VAGRANT_HOME/.ssh/authorized_keys
 chown -R $VAGRANT_USER:$VAGRANT_USER $VAGRANT_HOME/.ssh

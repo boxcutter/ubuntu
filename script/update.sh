246217e16f9d64819a818478721f373c84e2f3d3
Moujan Anna <moujan@annawake.com>
2015-06-06 11:45:12 -0700

    Experiment with updating package list during run

--- script/update.sh
+++ script/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
-if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
-	echo "==> Updating list of repositories"
-    # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
-    apt-get -y update
+echo "==> Updating list of repositories"
+# apt-get update does not actually perform updates, it just downloads and indexes the list of packages
+apt-get -y update
 
+if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Performing dist-upgrade (all packages and kernel)"
     apt-get -y dist-upgrade --force-yes
     reboot

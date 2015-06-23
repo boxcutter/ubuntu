8ee05c988ac4ae4650af7a0e23566ce01142a9b2
Moujan Anna <moujan@annawake.com>
2015-06-22 19:20:30 -0700

    Disable the release upgrader

--- script/update.sh
+++ script/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+# Disable the release upgrader
+echo "==> Disabling the release upgrader"
+sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
+
 echo "==> Updating list of repositories"
 # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
 apt-get -y update
--- test/ubuntu_spec.rb
+++ test/ubuntu_spec.rb
@@ -9,4 +9,9 @@ describe 'box' do
   it 'should make vagrant a member of the docker group', :if => has_docker do
     expect(user 'vagrant').to belong_to_group 'docker'
   end
+
+  it 'should turn off release upgrades' do
+    expect(file('/etc/update-manager/release-upgrades').content)
+      .to match(/Prompt=never/)
+  end
 end

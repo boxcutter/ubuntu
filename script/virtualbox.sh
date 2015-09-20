bceb01cd8040c8cd7b3dec8bb1f07a77c93aca91
Moujan Anna <moujan@annawake.com>
2015-09-20 11:18:39 -0700

    Detect VMware tools version to parametize vmware install

---
+++ script/virtualbox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+
+SSH_USER=${SSH_USERNAME:-vagrant}
+
+if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+    echo "==> Installing VirtualBox guest additions"
+    # Assuming the following packages are installed
+    # apt-get install -y linux-headers-$(uname -r) build-essential perl
+    # apt-get install -y dkms
+
+    VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
+    mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+    sh /mnt/VBoxLinuxAdditions.run
+    umount /mnt
+    rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
+    rm /home/${SSH_USER}/.vbox_version
+
+    if [[ $VBOX_VERSION = "4.3.10" ]]; then
+        ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+    fi
+fi
---
+++ script/vmware.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eux
+
+SSH_USERNAME=${SSH_USERNAME:-vagrant}
+
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+    echo "==> Installing VMware Tools"
+    # Assuming the following packages are installed
+    # apt-get install -y linux-headers-$(uname -r) build-essential perl
+
+    cd /tmp
+    mkdir -p /mnt/cdrom
+    mount -o loop /home/${SSH_USERNAME}/linux.iso /mnt/cdrom
+
+    VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
+    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
+    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
+    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
+    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
+    echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
+    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}" 
+
+    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
+    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
+        /tmp/vmware-tools-distrib/vmware-install.pl -d
+    else
+        /tmp/vmware-tools-distrib/vmware-install.pl --force-install
+    fi
+
+    rm /home/${SSH_USERNAME}/linux.iso
+    umount /mnt/cdrom
+    rmdir /mnt/cdrom
+    rm -rf /tmp/VMwareTools-*
+fi

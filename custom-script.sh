d397ed85f0e342a1538fd68015321b6681c73c38
Moujan Anna <moujan@annawake.com>
2015-10-24 11:25:09 -0700

    Add support for adding a custom script

---
+++ custom-script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Sample custom configuration script - add your own commands here
+# to add some additional commands for your environment
+#
+# For example:
+# yum install -y curl wget git tmux firefox xvfb

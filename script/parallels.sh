21606e46e58c7146f5bf18f9f8ee18b9f5faee95
Moujan Anna <moujan@annawake.com>
2015-10-24 22:52:38 -0700

    Install Parallels tools

--- script/parallels.sh
+++ script/parallels.sh
@@ -5,6 +5,7 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 if [[ $PACKER_BUILDER_TYPE =~ parallels ]]; then
     echo "==> Installing Parallels tools"
     mount -o loop /home/${SSH_USER}/prl-tools-lin.iso /mnt
+    /mnt/install --install-unattended-with-deps
     umount /mnt
     rm -rf /home/${SSH_USER}/prl-tools-lin.iso
     rm -f /home/${SSH_USER}/.prlctl_version

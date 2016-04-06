ea4b466a2871557c2719a214c441a7341d7a389f
Moujan Anna <moujan@annawake.com>
2016-04-05 20:56:13 -0700

    Add Ubuntu 16.04

--- bin/register_atlas.sh
+++ bin/register_atlas.sh
@@ -67,6 +67,9 @@ get_short_description() {
     RAW_VERSION=${RAW_VERSION%-desktop}
     PRETTY_VERSION=${RAW_VERSION:0:2}.${RAW_VERSION:2}
     case ${PRETTY_VERSION} in
+    16.04)
+        PRETTY_VERSION="16.04 Xenial Xerus"
+        ;;
     15.10)
         PRETTY_VERSION="15.10 Wily Werewolf"
         ;;
@@ -115,6 +118,9 @@ create_description() {
     RAW_VERSION=${RAW_VERSION%-desktop}
     PRETTY_VERSION=${RAW_VERSION:0:2}.${RAW_VERSION:2}
     case ${PRETTY_VERSION} in
+    16.04)
+        PRETTY_VERSION="16.04 Xenial Xerus"
+        ;;
     15.10)
         PRETTY_VERSION="15.10 Wily Werewolf"
         ;;

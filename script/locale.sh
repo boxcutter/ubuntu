126013fd4e5c005d272e7132f52cacd153fc5cdb
Boris Dušek <dusek@a11y.ltd.uk>
2016-04-24 12:19:36 +0200

    Unify 15.10 and 16.04 to use the master template
    By using the `boot_keysequence_emptycmdline` to override the initial
    boot key presses for 15.10 and 16.04, we can use the main template,
    `ubuntu.json`, for 15.10 and 16.04 as well. This significantly reduces
    the duplicity in all files (mainly duplicity of ubuntu-1510.json and
    ubuntu1604.json w.r.t. ubuntu.json).

--- script/locale.sh
+++ script/locale.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-echo "==> Setting LANG locale value"
-update-locale LANG=en_US.UTF-8
+
+echo "==> Checking version of Ubuntu"
+. /etc/lsb-release
+
+case "$DISTRIB_RELEASE" in
+    16.04)
+        echo "==> Fixing LANG locale value (necessary on $DISTRIB_RELEASE)"
+        update-locale LANG=en_US.UTF-8
+        ;;
+    *)
+        echo "==> Fixing LANG not necessary on $DISTRIB_RELEASE"
+        ;;
+esac

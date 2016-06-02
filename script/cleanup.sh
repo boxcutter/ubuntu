80bf1ff8f0aa0b7e639edcf57760d5037215dca9
Stefan Scherer <scherer_stefan@icloud.com>
2016-06-02 08:52:16 +0200

    Fix Ubuntu 16.04 for VMware

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -15,7 +15,19 @@ fi
 # Ubuntu 12.04 & 14.04
 if [ -d "/var/lib/dhcp" ]; then
     rm /var/lib/dhcp/*
-fi 
+fi
+
+. /etc/lsb-release
+if [[ $DISTRIB_RELEASE == 16.04 ]]; then
+  # from https://github.com/cbednarski/packer-ubuntu/blob/master/scripts-1604/vm_cleanup.sh#L9-L15
+  # When booting with Vagrant / VMware the PCI slot is changed from 33 to 32.
+  # Instead of eth0 the interface is now called ens33 to mach the PCI slot, so we
+  # need to change the networking scripts to enable the correct interface.
+  #
+  # NOTE: After the machine is rebooted Packer will not be able to reconnect
+  # (Vagrant will be able to) so make sure this is done in your final provisioner.
+  sed -i "s/ens33/ens32/g" /etc/network/interfaces
+fi
 
 # Add delay to prevent "vagrant reload" from failing
 echo "pre-up sleep 2" >> /etc/network/interfaces
--- script/vmware.sh
+++ script/vmware.sh
@@ -3,43 +3,9 @@
 SSH_USERNAME=${SSH_USERNAME:-vagrant}
 
 if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
-    echo "==> Installing VMware Tools"
-    # Assuming the following packages are installed
-    # apt-get install -y linux-headers-$(uname -r) build-essential perl
-
-    cd /tmp
-    mkdir -p /mnt/cdrom
-    mount -o loop /home/${SSH_USERNAME}/linux.iso /mnt/cdrom
-
-    VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
-    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
-    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
-    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
-    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
-    echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
-    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}" 
-
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
-    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
-        /tmp/vmware-tools-distrib/vmware-install.pl -d
-    else
-        /tmp/vmware-tools-distrib/vmware-install.pl -f
-    fi
-
-    rm /home/${SSH_USERNAME}/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
-    rm -rf /tmp/VMwareTools-*
-
-    VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
-    echo "==> Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}" 
-
-    echo "==> Checking version of Ubuntu"
-    . /etc/lsb-release
-    if [[ $DISTRIB_RELEASE == 15.10 ]]; then
-      echo "==> Applying workaround for Ubuntu 15.10"
-      sed -i.orig 's/^Exec=.*/Exec=env VMWARE_USE_SHIPPED_LIBS=1 \/usr\/bin\/vmare-user/' /etc/vmware-tools/vmware-user.desktop
-      cat /etc/vmware-tools/vmware-user.desktop
-    fi
+    echo "==> Installing Open VM Tools"
+    # Install open-vm-tools so we can mount shared folders
+    apt-get install -y open-vm-tools
+    # Add /mnt/hgfs so the mount works automatically with Vagrant
+    mkdir /mnt/hgfs
 fi

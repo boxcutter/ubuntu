4f275a201b608df6aac7d33ae87b849fb44a7d87
Torben Knerr <mail@tknerr.de>
2016-07-11 11:24:03 +0200

    Disable unattended upgrades via systemd that are enabled by default in 16.04 (see #73)

--- script/update.sh
+++ script/update.sh
@@ -4,6 +4,13 @@
 echo "==> Disabling the release upgrader"
 sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
 
+# Disable unattended upgrades via systemd on 16.04, see issue #73
+if [[ $DISTRIB_RELEASE == 16.04 ]]; then
+    echo "==> Disabling unattended upgrades via systemd"
+    systemctl disable apt-daily.service
+    systemctl disable apt-daily.timer
+fi
+
 echo "==> Updating list of repositories"
 # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
 apt-get -y update

c93fd5aa0f3dd7295df08f55707d4682a4b54ea1
Mischa Taylor <mischataylor@gmail.com>
2016-08-21 18:30:08 -0700

    Disable screen blanking via xdg autostart

--- script/desktop.sh
+++ script/desktop.sh
@@ -44,3 +44,16 @@ elif [[ $DISTRIB_RELEASE == 14.04 || $DISTRIB_RELEASE == 15.04 || $DISTRIB_RELEA
     echo "[SeatDefaults]" >> $LIGHTDM_CONFIG
     echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
 fi
+
+echo "==> Disabling screen blanking"
+NODPMS_CONFIG=/etc/xdg/autostart/nodpms.desktop
+echo "[Desktop Entry]" >> $NODPMS_CONFIG
+echo "Type=Application" >> $NODPMS_CONFIG
+echo "Exec=xset -dpms s off s noblank s 0 0 s noexpose" >> $NODPMS_CONFIG
+echo "Hidden=false" >> $NODPMS_CONFIG
+echo "NoDisplay=false" >> $NODPMS_CONFIG
+echo "X-GNOME-Autostart-enabled=true" >> $NODPMS_CONFIG
+echo "Name[en_US]=nodpms" >> $NODPMS_CONFIG
+echo "Name=nodpms" >> $NODPMS_CONFIG
+echo "Comment[en_US]=" >> $NODPMS_CONFIG
+echo "Comment=" >> $NODPMS_CONFIG

d73732d0904988fe19eae9ddb58e18c86e1a113a
Torben Knerr <mail@tknerr.de>
2017-04-11 00:26:54 +0200

    install the open-vm-tools-desktop package for the ubuntu desktop variants

--- script/vmware.sh
+++ script/vmware.sh
@@ -6,6 +6,12 @@ function install_open_vm_tools {
     echo "==> Installing Open VM Tools"
     # Install open-vm-tools so we can mount shared folders
     apt-get install -y open-vm-tools
+
+    # Install open-vm-tools-desktop so we can copy/paste, resize, etc..
+    if [[ "$DESKTOP" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
+        apt-get install -y open-vm-tools-desktop
+    fi
+
     # Add /mnt/hgfs so the mount works automatically with Vagrant
     mkdir /mnt/hgfs
 }

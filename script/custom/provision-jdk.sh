b0933b9e4643dbddc19d01d9626ba72e2abf4837
Jeroen Willemsen <jwillemsen@xebia.com>
2019-04-19 14:01:07 +0200

    switch to openjdk8 jre

--- script/custom/provision-jdk.sh
+++ script/custom/provision-jdk.sh
@@ -32,10 +32,11 @@ set -e
 
 echo "Provisioning runtime next to the JDK"
 sleep 60
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-
-apt-get install -y software-properties-common
-add-apt-repository ppa:webupd8team/java -y
-apt-get update -y
-apt-get install oracle-java8-installer -y
-apt-get install ant -y
+# echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#
+# apt-get install -y software-properties-common
+# add-apt-repository ppa:webupd8team/java -y
+# apt-get update -y
+# apt-get install oracle-java8-installer -y
+# apt-get install ant -y
+sudo apt-get install openjdk-8-jre

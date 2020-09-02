a8b7f3690b523e6bb606453c4f0a271e7a772a99
Jeroen Willemsen <jwillemsen@xebia.com>
2020-09-02 22:25:34 +0200

    Updates to do more infra

--- script/custom/provision-burp.sh
+++ script/custom/provision-burp.sh
@@ -4,7 +4,8 @@ echo "Provisioning burp"
 cd tools
 mkdir burp
 cd burp
-wget "https://portswigger.net/burp/releases/initiatedownload?product=community&version=2020.6&type=Jar"
+#TODO: FIX THE ISSUE BELOW!
+wget "https://portswigger.net/burp/releases/initiatedownload?product=community&version=2020.8.1&type=Jar"
 mv * burp.jar
 
 echo "java -jar burp.jar" > burp.sh
---
+++ script/custom/provision-inspec.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+echo "provisioning inspec"
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+
+cd workspace
+wget https://github.com/commjoen/inspec_intro/archive/master.zip
+unzip master.zip
+rm master.zip
+mv inspec_intro-master inspec_intro
+
+echo "provisioning lynis"
+sudo apt-get install lynis
+
+echo "provisioning cvescan"
+sudo snap install cvescan
+
+echo "provisioning openscap"
+cd ~/tools
+mkdir openscap
+cd openscap
+sudo apt-get install libopenscap8
+echo "wget https://people.canonical.com/~ubuntu-security/oval/com.ubuntu.$(lsb_release -cs).cve.oval.xml.bz2" >> openscap.sh
+ehco "bunzip2 com.ubuntu.$(lsb_release -cs).cve.oval.xml.bz2" >> openscap.sh
+echo "oscap oval eval --report report.htm com.ubuntu.$(lsb_release -cs).cve.oval.xml" >> openscap.sh
+chmod +x openscap.sh
